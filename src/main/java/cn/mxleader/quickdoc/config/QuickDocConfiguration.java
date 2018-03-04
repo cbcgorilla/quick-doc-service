5b3bfbfcef48233154f8a8616e156545fb27c852
Michael Chen <cbcgorilla@gmail.com>
2018-03-04 23:12:33 +0800

    修复CoreUI界面BUG

--- src/main/java/cn/mxleader/quickdoc/config/QuickDocConfiguration.java
+++ src/main/java/cn/mxleader/quickdoc/config/QuickDocConfiguration.java
@@ -62,13 +62,14 @@ public class QuickDocConfiguration {
 /*
     @Bean
     public MultipartResolver multipartResolver(){
-        CommonsMultipartResolver resolver = new MxLeaderMultipartResolver();
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setDefaultEncoding("UTF-8");
         resolver.setResolveLazily(true);//resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
         resolver.setMaxInMemorySize(40960);
         resolver.setMaxUploadSize(1024*1024*1024);//上传文件大小 1G 1024*1024*1024
         return resolver;
-    }*/
+    }
+*/
 
     @Bean
     CommandLineRunner initConfigurationData(ReactiveUserServiceImpl reactiveUserService,
